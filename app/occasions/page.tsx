f6e84ae789adeb9687ac6e488c060c4b0be73047
Abdelghani Adel <abdelghani.adel@outlook.com>
2024-11-17 16:40:20 +0200

    Landing Page

--- app/occasions/page.tsx
+++ app/occasions/page.tsx
@@ -5,25 +5,30 @@ import products from "@/public/data/products.json";
 
 const Page = () => {
   const [filteredProducts, setFilteredProducts] = useState<any[]>([]);
-  const occasion = new URLSearchParams(window.location.search).get("occasion"); // Get category from URL arguments
+  let occasion = "";
 
   useEffect(() => {
-    // Filter products based on category when it changes
-    if (occasion) {
-      const filtered = products.filter((product) =>
-        product.occasions.includes(occasion)
-      );
-      setFilteredProducts(filtered);
-    } else {
-      setFilteredProducts(products); // If no category, show all products
+    if (typeof window !== "undefined") {
+      const occasion = new URLSearchParams(window.location.search).get(
+        "occasion"
+      ); // Get category from URL arguments
+      // Filter products based on category when it changes
+      if (occasion) {
+        const filtered = products.filter((product) =>
+          product.occasions.includes(occasion)
+        );
+        setFilteredProducts(filtered);
+      } else {
+        setFilteredProducts(products); // If no category, show all products
+      }
     }
-  }, [occasion, products]); // Re-run the filter if products or category change
+  }, [products]); // Re-run the filter if products or category change
 
   return (
     <div className="my-5">
-      <h1 className="m-5 text-4xl text-center font-bold text-main">
+      {/* <h1 className="m-5 text-4xl text-center font-bold text-main">
         {occasion}
-      </h1>
+      </h1> */}
       <ProductsList list={filteredProducts} />
     </div>
   );
